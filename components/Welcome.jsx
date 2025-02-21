f58097263ddb3d1a71f778c0859cb0f8a8dd225f
jeff-dev-art <udohjephthah@gmail.com>
2025-02-21 23:50:17 +0100

    update

--- components/Welcome.jsx
+++ components/Welcome.jsx
@@ -1,7 +1,6 @@
 'use client';
 import Image from 'next/image';
 import { Button } from './ui/button';
-import Start from './Start';
 import { useState } from 'react';
 import translate from '@/lib/language';
 import summarise from '@/lib/nonStreamSummarizer';
